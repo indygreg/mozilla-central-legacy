37063fc5a27777ef33a93a99df43dbbd34d5d557
Gregory Szorc <gps@mozilla.com>
2011-10-29 13:14:33 -0700

    lazy-load Makefiles; add statement APIs to Makefile

--- build/buildparser/makefile.py
+++ build/buildparser/makefile.py
@@ -63,18 +63,37 @@ class Makefile(object):
 
         self.filename = filename
         self.dir      = os.path.dirname(filename)
-        self.makefile = pymake.data.Makefile(workdir=self.dir)
 
-        self.makefile.include(filename)
-        self.makefile.finishparsing()
+        # Each Makefile instance can look at two sets of data, the low-level
+        # parser output or the high-level "this is what's in a Makefile". Both
+        # data sets are lazy-loaded for performance reasons.
+
+        # The Makefile is lazy loaded because under some conditions loading the
+        # Makefile can cause PyMake to explode. I [gps] think that PyMake is
+        # trying to evaluate variables during loading and the PyMake
+        # environment isn't set up properly to allow the exec(), etc calls
+        # to succeed. We either need to produce a proper environment for PyMake
+        # or fix PyMake so it doens't die a horrible death. Until then, we lazy
+        # load the actual Makefile.
+        self.makefile = None
+
+        # The following are caches for low-level statement data.
+        self.statements = None
+        self.own_variables = None
 
     def has_variable(self, name):
         '''Determines whether a named variable is defined.'''
+        if self.makefile is None:
+            self._load_makefile()
+
         v = self.makefile.variables.get(name, True)[2]
         return v is not None
 
     def get_variable_string(self, name):
         '''Obtain a named variable as a string.'''
+        if self.makefile is None:
+            self._load_makefile()
+
         v = self.makefile.variables.get(name, True)[2]
         if v is None:
             return None
@@ -83,29 +102,90 @@ class Makefile(object):
 
     def get_variable_split(self, name):
         '''Obtain a named variable as a list.'''
+        if self.makefile is None:
+            self._load_makefile()
+
         v = self.makefile.variables.get(name, True)[2]
         if v is None:
             return []
 
         return v.resolvesplit(self.makefile, self.makefile.variables)
 
+    def get_statements(self):
+        '''Obtain all the low-level PyMake-parsed statements from the file.'''
+        self._parse_file()
+
+        return self.statements
+
+    def get_own_variable_names(self, ignore_conditional=False):
+        '''Returns a list of variables defined by the Makefile itself.
+
+        This looks at the low-level parsed Makefile, before including other
+        files, and determines which variables are defined.
+
+        ignore_conditional can be used to filter out variables defined inside
+        a conditional (e.g. #ifdef). By default, all variables are returned,
+        even the ones inside conditionals that may not be evaluated.
+        '''
+
+        # Lazy-load and cache.
+        if self.own_variables is None:
+            self._load_own_variables()
+
+        if ignore_conditional:
+            return [n for n in self.own_variables.keys()]
+        else:
+            return [k for k, v in self.own_variables.iteritems() if not v[1]]
+
+    def _load_makefile(self):
+        self.makefile = pymake.data.Makefile(workdir=self.dir)
+        self.makefile.include(self.filename)
+        self.makefile.finishparsing()
+
+    def _parse_file(self):
+        if self.statements is None:
+            self.statements = pymake.parser.parsefile(self.filename)
+
+    def _load_own_variables(self):
+        self._parse_file()
+
+        # Name to tuple of ( number of definitions, defined anywhere in condition )
+        vars = {}
+
+        # We also need to collect SetVariable statements inside condition
+        # blocks. Condition blocks just contain lists of statements, so we
+        # recursively examine these all while adding to the same list.
+        def examine_statements(statements, in_condition):
+            for statement in statements:
+                if isinstance(statement, pymake.parserdata.SetVariable):
+                    vnameexp = statement.vnameexp
+                    if isinstance(vnameexp, pymake.data.StringExpansion):
+                        name = vnameexp.s
+
+                        if not name in vars:
+                            vars[name] = (1, in_condition)
+                        else:
+                            has_condition = vars[name][1]
+                            if not has_condition:
+                                has_condition = in_condition
+
+                            vars[name] = ( vars[name][0] + 1, has_condition )
+                    else:
+                        raise Exception('Unhandled SetVariable vnameexp: %s' % type(vnameexp))
+
+                elif isinstance(statement, pymake.parserdata.ConditionBlock):
+                    for group in statement:
+                        examine_statements(group[1], True)
+
+        examine_statements(self.statements, False)
+
+        self.own_variables = vars
 
 class MozillaMakefile(Makefile):
     '''A Makefile with knowledge of Mozilla's build system.'''
 
     def __init__(self, filename):
         Makefile.__init__(self, filename)
-
-        self.module = self.get_module()
-
-        depth = self.get_variable_string('DEPTH')
-        if not depth:
-            depth = self.get_variable_string('MOD_DEPTH')
-
-        self.objtop = os.path.abspath(os.path.join(self.dir, depth))
-        absdir = os.path.abspath(self.dir)
-
-        self.reldir = absdir[len(self.objtop)+1:]
 
     def get_dirs(self):
         dirs = self.get_variable_split('DIRS')
@@ -121,6 +201,18 @@ class MozillaMakefile(Makefile):
 
     def get_library(self):
         return self.get_variable_string('LIBRARY')
+
+    def get_reldir(self):
+        absdir = os.path.abspath(self.dir)
+
+        return absdir[len(self.objtop)+1:]
+
+    def get_objtop(self):
+        depth = self.get_variable_string('DEPTH')
+        if not depth:
+            depth = self.get_variable_string('MOD_DEPTH')
+
+        return os.path.abspath(os.path.join(self.dir, depth))
 
     def is_xpidl_module(self):
         return self.has_variable('XPIDL_MODULE')
@@ -144,7 +236,7 @@ class MozillaMakefile(Makefile):
         return self.get_variable_string('DEFINES')
 
     def get_transformed_reldir(self):
-        return self.reldir.replace('\\', '_').replace('/', '_')
+        return self.get_reldir().replace('\\', '_').replace('/', '_')
 
     def get_library_info(self):
         library = self.get_library()
@@ -165,8 +257,8 @@ class MozillaMakefile(Makefile):
             'name':            library,
             'normalized_name': self.get_transformed_reldir(),
             'dir':             self.dir,
-            'reldir':          self.reldir,
-            'objtop':          self.objtop,
+            'reldir':          self.get_reldir(),
+            'objtop':          self.get_objtop(),
             'defines':         self.get_defines(),
             'cppsrcs':         self.get_cpp_sources(),
             'xpidlsrcs':       self.get_variable_split('XPIDLSRCS'),
@@ -234,17 +326,20 @@ class MakefileCritic(Critic):
         pass
 
     def critique(self, filename):
-        if not os.path.exists(filename):
-            raise 'file does not exist: %s' % filename
+        makefile = MozillaMakefile(filename)
 
-        statements = pymake.parser.parsefile(filename)
+        statements = makefile.get_statements()
 
         state = {
-            'filename':   filename,
-            'statements': statements
+            'filename':       filename,
+            'statements':     statements,
+            'variable_names': makefile.get_own_variable_names(),
         }
 
         for critique in self.critique_statements(state):
+            yield (filename, critique[0][0], critique[0][1], critique[1])
+
+        for critique in self.critique_variable_names(state):
             yield (filename, critique[0][0], critique[0][1], critique[1])
 
     def critique_statements(self, state):
@@ -270,11 +365,8 @@ class MakefileCritic(Critic):
                 # TODO do something
                 pass
             elif isinstance(statement, pymake.parserdata.SetVariable):
-                vnameexp = statement.vnameexp
-                if isinstance(vnameexp, pymake.data.StringExpansion):
-                    variable_names.append(vnameexp.s)
-                else:
-                    yield (self.CRITIC_ERROR, 'Unhandled vnamexp type: %s' % type(vnameexp))
+                # TODO do something
+                pass
             elif isinstance(statement, pymake.parserdata.StaticPatternRule):
                 # TODO do anything?
                 pass
@@ -284,10 +376,6 @@ class MakefileCritic(Critic):
             else:
                 yield (self.CRITIC_ERROR, 'Unhandled statement type: %s' % type(statement))
                 #pass
-
-        state['variable_names'] = variable_names
-        for critique in self.critique_variable_names(state):
-            yield critique
 
     def critique_variable_names(self, state):
         '''Critique variable names.'''
