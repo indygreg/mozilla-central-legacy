e9a7429a2cd6ef34f6130b26eb4182feae12b470
Gregory Szorc <gregory.szorc@gmail.com>
2011-11-20 21:30:01 -0800

    move MozillaMakefile to extractor.py

--- build/buildparser/extractor.py
+++ build/buildparser/extractor.py
@@ -439,6 +439,345 @@ class BuildSystem(object):
             self.callback(action, params, formatter,
                           important=important, error=error)
 
+class MozillaMakefile(Makefile):
+    '''A Makefile with knowledge of Mozilla's build system.'''
+
+    '''Traits that can identify a Makefile'''
+    MODULE       = 1
+    LIBRARY      = 2
+    DIRS         = 3
+    XPIDL        = 4
+    EXPORTS      = 5
+    TEST         = 6
+    PROGRAM      = 7
+
+    '''Variables common in most Makefiles that aren't really that special.
+
+    This list is used to help identify variables we don't do anything with.'''
+    COMMON_VARIABLES = [
+        'DEPTH',            # Defined at top of file
+        'topsrcdir',        # Defined at top of file
+        'srcdir',           # Defined at top of file
+        'VPATH',            # Defined at top of file
+        'relativesrcdir',   # Defined at top of file. # TODO is this used by anything?
+        'DIRS',             # Path traversal
+        'PARALLEL_DIRS',    # Path traversal
+        'TOOL_DIRS',        # Path traversal
+    ]
+
+    '''This list tracks all variables that are still in the wild but aren't used'''
+    UNUSED_VARIABLES = []
+
+    def __init__(self, filename):
+        Makefile.__init__(self, filename)
+
+        self.traits = None
+
+    def get_traits(self):
+        '''Obtain traits of the Makefile.
+
+        Traits are recognized patterns that invoke special functionality in
+        Mozilla's Makefiles. Traits are identified by the presence of specific
+        named variables.'''
+        if self.traits is not None:
+            return self.traits
+
+        self.traits = set()
+        variable_names = self.get_own_variable_names(include_conditionals=True)
+        for name in variable_names:
+            if name == 'MODULE':
+                self.traits.add(self.MODULE)
+            elif name == 'LIBRARY_NAME':
+                self.traits.add(self.LIBRARY)
+            elif name == 'DIRS' or name == 'PARALLEL_DIRS':
+                self.traits.add(self.DIRS)
+            elif name in ('XPIDL_MODULE', 'XPIDLSRCS', 'SDK_XPIDLSRCS'):
+                self.traits.add(self.XPIDL)
+            elif name in ('EXPORTS', 'EXPORTS_NAMESPACES'):
+                self.traits.add(self.EXPORTS)
+            elif name in ('_TEST_FILES', 'XPCSHELL_TESTS', '_BROWSER_TEST_FILES', '_CHROME_TEST_FILES'):
+                self.traits.add(self.TEST)
+            elif name in ('PROGRAM'):
+                self.traits.add(self.PROGRAM)
+
+        return self.traits
+
+    def get_dirs(self):
+        dirs = self.get_variable_split('DIRS')
+        dirs.extend(self.get_variable_split('PARALLEL_DIRS'))
+
+        return dirs
+
+    def is_module(self):
+        return self.MODULE in self.get_traits()
+
+    def is_xpidl_module(self):
+        return self.XPIDL_MODULE in self.get_traits()
+
+    def get_module(self):
+        return self.get_variable_string('MODULE')
+
+    def get_reldir(self):
+        absdir = os.path.abspath(self.dir)
+
+        return absdir[len(self.get_objtop())+1:]
+
+    def get_objtop(self):
+        depth = self.get_variable_string('DEPTH')
+        if not depth:
+            depth = self.get_variable_string('MOD_DEPTH')
+
+        return os.path.abspath(os.path.join(self.dir, depth))
+
+    def get_top_source_dir(self):
+        return self.get_variable_string('topsrcdir')
+
+    def get_source_dir(self):
+        return self.get_variable_string('srcdir')
+
+    def get_transformed_reldir(self):
+        return self.get_reldir().replace('\\', '_').replace('/', '_')
+
+    def get_library_info(self):
+        '''Obtain information for the library defined by this Makefile.
+
+        Returns a data.LibraryInfo instance'''
+        l = data.LibraryInfo(self)
+
+        # It is possible for the name to be not defined if the trait was
+        # in a conditional that wasn't true.
+        l.add_used_variable('LIBRARY_NAME')
+        name = self.get_variable_string('LIBRARY_NAME')
+        l.name = name
+
+        l.add_used_variable('DEFINES')
+        for define in self.get_variable_split('DEFINES'):
+            if define[0:2] == '-D':
+                l.defines.add(define[2:])
+            else:
+                l.defines.add(define)
+
+        l.add_used_variable('CFLAGS')
+        for f in self.get_variable_split('CFLAGS'):
+            l.c_flags.add(f)
+
+        l.add_used_variable('CXXFLAGS')
+        for f in self.get_variable_split('CXXFLAGS'):
+            l.cxx_flags.add(f)
+
+        l.add_used_variable('CPPSRCS')
+        for f in self.get_variable_split('CPPSRCS'):
+            l.cpp_sources.add(f)
+
+        # LIBXUL_LIBRARY implies static library generation and presence in
+        # libxul.
+        l.add_used_variable('LIBXUL_LIBRARY')
+        if self.has_own_variable('LIBXUL_LIBRARY'):
+            l.is_static = True
+
+        # FORCE_STATIC_LIB forces generation of a static library
+        l.add_used_variable('FORCE_STATIC_LIB')
+        if self.has_own_variable('FORCE_STATIC_LIB'):
+            l.is_static = True
+
+        l.add_used_variable('FORCE_SHARED_LIB')
+        if self.has_own_variable('FORCE_SHARED_LIB'):
+            l.is_shared = True
+
+        l.add_used_variable('USE_STATIC_LIBS')
+        if self.has_own_variable('USE_STATIC_LIBS'):
+            l.use_static_libs = True
+
+        # IS_COMPONENT is used for verification. It also has side effects for
+        # linking flags.
+        l.add_used_variable('IS_COMPONENT')
+        if self.has_own_variable('IS_COMPONENT'):
+            l.is_component = self.get_variable_string('IS_COMPONENT') == '1'
+
+        l.add_used_variable('EXPORT_LIBRARY')
+        if self.has_own_variable('EXPORT_LIBRARY'):
+            l.export_library = self.get_variable_string('EXPORT_LIBRARY') == '1'
+
+        l.add_used_variable('INCLUDES')
+        for s in self.get_variable_split('INCLUDES'):
+            if s[0:2] == '-I':
+                l.includes.add(s[2:])
+            else:
+                l.includes.add(s)
+
+        l.add_used_variable('LOCAL_INCLUDES')
+        for s in self.get_variable_split('LOCAL_INCLUDES'):
+            if s[0:2] == '-I':
+                l.local_includes.add(s[2:])
+            else:
+                l.local_includes.add(s)
+
+        # SHORT_LIBNAME doesn't appears to be used, but we preserve it anyway.
+        l.add_used_variable('SHORT_LIBNAME')
+        if self.has_own_variable('SHORT_LIBNAME'):
+            l.short_libname = self.get_variable_string('SHORT_LIBNAME')
+
+        l.add_used_variable('SHARED_LIBRARY_LIBS')
+        for lib in self.get_variable_split('SHARED_LIBRARY_LIBS'):
+            l.shared_library_libs.add(lib)
+
+        return l
+
+    def get_data_objects(self):
+        '''Retrieve data objects derived from the Makefile.
+
+        This is the main function that extracts metadata from individual
+        Makefiles and turns them into Python data structures.
+
+        This method emits a set of MakefileDerivedObjects which describe the
+        Makefile. These objects each describe an individual part of the
+        build system, e.g. libraries, IDL files, tests, etc. These emitted
+        objects can be fed into another system for conversion to another
+        build system, fed into a monolithic data structure, etc.
+        '''
+        misc = data.MiscInfo(self)
+        tracker = data.UsedVariableInfo(self)
+        for v in self.COMMON_VARIABLES:
+            tracker.add_used_variable(v)
+
+        for v in self.UNUSED_VARIABLES:
+            tracker.add_used_variable(v)
+
+        traits = self.get_traits()
+
+        if self.MODULE in traits:
+            tracker.add_used_variable('MODULE')
+            # TODO emit MakefileDerivedObject instance
+            #tree.register_module(self.get_module(), self.dir)
+
+        if self.LIBRARY in traits:
+            li = self.get_library_info()
+            yield li
+
+        if self.PROGRAM in traits:
+            # TODO capture programs. Executables and libraries are two sides of
+            # the same coin. How should this be captured?
+            pass
+
+        # MODULE_NAME is only used for error checking, it appears.
+        tracker.add_used_variable('MODULE_NAME')
+
+        # EXPORTS and friends holds information on what files to copy
+        # to an output directory.
+        if self.EXPORTS in traits:
+            exports = data.ExportsInfo(self)
+            exports.add_used_variable('EXPORTS')
+            for export in self.get_variable_split('EXPORTS'):
+                exports.add_export(export, namespace=None)
+
+            exports.add_used_variable('EXPORTS_NAMESPACES')
+            for namespace in self.get_variable_split('EXPORTS_NAMESPACES'):
+                varname = 'EXPORTS_%s' % namespace
+                exports.add_used_variable(varname)
+                for s in self.get_variable_split(varname):
+                    exports.add_export(s, namespace=namespace)
+
+            yield exports
+
+        # XP IDL file generation
+        if self.XPIDL in traits:
+            idl = data.XPIDLInfo(self)
+            idl.add_used_variable('XPIDL_MODULE')
+            idl.add_used_variable('MODULE')
+            if self.has_own_variable('XPIDL_MODULE'):
+                idl.module = self.get_variable_string('XPIDL_MODULE')
+            elif self.has_own_variable('MODULE'):
+                idl.module = self.get_variable_string('MODULE')
+            else:
+                raise Exception('XPIDL trait without XPIDL_MODULE or MODULE defined')
+
+            idl.add_used_variable('XPIDLSRCS')
+            if self.has_own_variable('XPIDLSRCS'):
+                for f in self.get_variable_split('XPIDLSRCS'):
+                    idl.sources.add(f)
+
+            # rules.mk merges SDK_XPIDLSRCS together, so we treat as the same
+            if self.has_own_variable('SDK_XPIDLSRCS'):
+                for f in self.get_variable_split('SDK_XPIDLSRCS'):
+                    idl.sources.add(f)
+
+            yield idl
+
+        # Test definitions
+        if self.TEST in traits:
+            ti = data.TestInfo(self)
+
+            # Regular test files
+            ti.add_used_variable('_TEST_FILES')
+            if self.has_own_variable('_TEST_FILES'):
+                for f in self.get_variable_split('_TEST_FILES'):
+                    ti.test_files.add(f)
+
+            # Identifies directories holding xpcshell test files
+            ti.add_used_variable('XPCSHELL_TESTS')
+            if self.has_own_variable('XPCSHELL_TESTS'):
+                for dir in self.get_variable_split('XPCSHELL_TESTS'):
+                    ti.xpcshell_test_dirs.add(dir)
+
+            # Files for browser tests
+            ti.add_used_variable('_BROWSER_TEST_FILES')
+            if self.has_own_variable('_BROWSER_TEST_FILES'):
+                for f in self.get_variable_split('_BROWSER_TEST_FILES'):
+                    ti.browser_test_files.add(f)
+
+            # Files for chrome tests
+            ti.add_used_variable('_CHROME_TEST_FILES')
+            if self.has_own_variable('_CHROME_TEST_FILES'):
+                for f in self.get_variable_split('_CHROME_TEST_FILES'):
+                    ti.chrome_test_files.add(f)
+
+            yield ti
+
+        misc.add_used_variable('GRE_MODULE')
+        if self.has_own_variable('GRE_MODULE'):
+            misc.is_gre_module = True
+
+        #misc.add_used_variable('PLATFORM_DIR')
+        #for d in self.get_variable_split('PLATFORM_DIR'):
+        #    misc.platform_dirs.add(d)
+
+        #misc.add_used_variable('CHROME_DEPS')
+        #for d in self.get_variable_split('CHROME_DEPS'):
+        #    misc.chrome_dependencies.add(d)
+
+        # DEFINES is used by JarMaker too. Unfortunately, we can't detect
+        # when to do JarMaker from Makefiles (bug 487182 might fix it), so
+        # we just pass it along.
+        misc.add_used_variable('DEFINES')
+        if self.has_own_variable('DEFINES'):
+            for define in self.get_variable_split('DEFINES'):
+                if define[0:2] == '-D':
+                    misc.defines.add(define[2:])
+                else:
+                    misc.defines.add(define)
+
+        # TODO add an info object for JavaScript-related
+        misc.add_used_variable('EXTRA_JS_MODULES')
+        if self.has_own_variable('EXTRA_JS_MODULES'):
+            for js in self.get_variable_split('EXTRA_JS_MODULES'):
+                misc.extra_js_module.add(js)
+
+        misc.add_used_variable('EXTRA_COMPONENTS')
+        if self.has_own_variable('EXTRA_COMPONENTS'):
+            for c in self.get_variable_split('EXTRA_COMPONENTS'):
+                misc.extra_components.add(c)
+
+        misc.add_used_variable('GARBAGE')
+        if self.has_own_variable('GARBAGE'):
+            for g in self.get_variable_split('GARBAGE'):
+                misc.garbage.add(g)
+
+        misc.included_files = [t[0] for t in self.statements.includes]
+
+        yield tracker
+        yield misc
+
+
 class ObjectDirectoryParser(object):
     '''A parser for an object directory.
 
@@ -515,7 +854,7 @@ class ObjectDirectoryParser(object):
 
         top_makefile_path = os.path.join(directory, 'Makefile')
 
-        self.top_makefile = makefile.MozillaMakefile(top_makefile_path)
+        self.top_makefile = MozillaMakefile(top_makefile_path)
         self.top_source_dir = self.top_makefile.get_top_source_dir()
 
         # The following hold data once we are parsed.
@@ -613,7 +952,7 @@ class ObjectDirectoryParser(object):
         assert(os.path.isabs(path))
 
         self.handled_makefile_paths.add(path)
-        m = makefile.MozillaMakefile(path)
+        m = MozillaMakefile(path)
 
         own_variables = set(m.get_own_variable_names(include_conditionals=True))
 
@@ -726,7 +1065,7 @@ class ObjectDirectoryParser(object):
 
     def collect_makefile_metadata(self, m):
         '''Collects metadata from a Makefile into memory.'''
-        assert(isinstance(m, makefile.MozillaMakefile))
+        assert(isinstance(m, MozillaMakefile))
 
         own_variables = set(m.get_own_variable_names(include_conditionals=True))
         own_variables_unconditional = set(m.get_own_variable_names(include_conditionals=False))
