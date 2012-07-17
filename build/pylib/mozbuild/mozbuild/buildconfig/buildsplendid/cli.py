9aafb3cfd2ecd49b62b6cd330b2bc5374d17b4f4
Gregory Szorc <gps@mozilla.com>
2012-07-16 22:24:41 -0700

    Remove deprecated code from old build splendid CLI

--- build/pylib/mozbuild/mozbuild/buildconfig/buildsplendid/cli.py
+++ build/pylib/mozbuild/mozbuild/buildconfig/buildsplendid/cli.py
@@ -3,50 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# This module provides functionality for the command-line build tool
-# (build.py). It is packaged as a module just because.
-
-from . import buildsystem
-from . import config
-from . import extractor
-from . import makefile
-
-import argparse
-import json
-import logging
-import os.path
-import pymake.parser
-import sys
-import time
-
 class BuildTool(object):
-    """Contains code for the command-line build.py interface."""
-
-    ACTIONS = {
-        'actions': 'Show all actions that can be performed.',
-        'build': 'Performs all steps necessary to perform a build.',
-        'bxr': 'Generate Build Cross Reference HTML file describing the build system.',
-        'configure': 'Run autoconf and ensure your build environment is proper.',
-        'format-makefile': 'Print a makefile (re)formatted.',
-        'help': 'Show full help documentation.',
-        'makefiles': 'Generate Makefiles to build the project.',
-        'settings': 'Sets up your build settings.',
-        'unittest': 'Run the unit tests for the build system code',
-        'wipe': 'Wipe your output directory and force a full rebuild.',
-    }
-
-    def bxr(self, bs, output):
-        """Generate BXR.
-
-        Arguments:
-
-          output -- File object to write output to.
-        """
-        # We lazy import because we don't want a dependency on Mako. If that
-        # package is every included with the source tree.
-        from . import bxr
-        bxr.generate_bxr(bs.config, output)
-
     def format_makefile(self, bs, format, filename=None, input=None,
                         output=None, strip_ifeq=False):
         """Format Makefiles different ways.
