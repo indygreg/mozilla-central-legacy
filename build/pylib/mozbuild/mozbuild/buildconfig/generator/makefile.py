909eea17e3e840ff9b00023e7b76a5e1681a9874
Gregory Szorc <gps@mozilla.com>
2012-07-23 23:05:22 -0700

    Add deprecation notice to makefile generator

--- build/pylib/mozbuild/mozbuild/buildconfig/generator/makefile.py
+++ build/pylib/mozbuild/mozbuild/buildconfig/generator/makefile.py
@@ -2,8 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# This file contains code for turning the Python build system data structures
-# into Makefiles.
+# THIS FILE IS DEPRECATED AND HAS BEEN REPLACED BY BACKENDS.
 
 import os
 import traceback
