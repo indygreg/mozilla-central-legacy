d08078b75ea6b878839e90b16139ebab4c4cb0fc
Gregory Szorc <gps@mozilla.com>
2012-08-14 23:53:42 -0700

    Lift FileAvoidWrite from ConfigStatus.py into util

--- python/mozbuild-bs/mozbuild/util.py
+++ python/mozbuild-bs/mozbuild/util.py
@@ -6,6 +6,9 @@
 # in particular.
 
 import hashlib
+import os
+
+from StringIO import StringIO
 
 
 def hash_file(path):
@@ -25,3 +28,44 @@ def hash_file(path):
             h.update(data)
 
     return h.hexdigest()
+
+
+class FileAvoidWrite(StringIO):
+    """file-like object that buffers its output and only writes it to disk
+    if the new contents are different from what the file may already contain.
+
+    This was shamelessly stolen from ConfigStatus.py.
+    """
+    def __init__(self, filename):
+        self.filename = filename
+        StringIO.__init__(self)
+
+    def close(self):
+        buf = self.getvalue()
+        StringIO.close(self)
+
+        try:
+            fh = open(self.filename, 'rU')
+        except IOError:
+            pass
+        else:
+            try:
+                if fh.read() == buf:
+                    return
+            except IOError:
+                pass
+            finally:
+                fh.close()
+
+        parent_directory = os.path.dirname(self.filename)
+        if not os.path.exists(parent_directory):
+            os.makedirs(parent_directory)
+
+        with open(self.filename, 'w') as fh:
+            fh.write(buf)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
