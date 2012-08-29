878fcb08e44676c130f424e4f7e9a7e159925efe
Gregory Szorc <gps@mozilla.com>
2012-08-29 00:16:08 -0700

    Add example script showing how to parse the build config

---
+++ buildread.py
@@ -0,0 +1,20 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# This is a sample script showing how to consume the build.mozbuild
+# files. Run it with the virtualenv-configured python binary from your
+# objdir and it should just work.
+#
+# e.g. ./objdir/_virtualenv/bin/python buildread.py
+
+
+import os
+import sys
+
+import mozconfig
+
+from mozbuild.frontend.reader import BuildReader
+
+reader = BuildReader(mozconfig)
+reader.read()
